013a9b793746dee5fd25f4a39ba4c0d14d218bfc
vish9151 <84173784+vishal9151@users.noreply.github.com>
2023-10-05 15:46:49 +0530

    coomit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+// import Stopwatch
+import Stopwatch from "./Stopwatch";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Stopwatch />
+    </div>
+  );
+}
---
+++ src/Stopwatch.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+
+function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRunning]);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  };
+
+  return (
+    <div className="stopwatch">
+      <h1>Stopwatch</h1>
+      <div className="time">{elapsedTime} seconds</div>
+      <br />
+      <div className="buttons">
+        <button onClick={startTimer} disabled={isRunning}>
+          Start
+        </button>
+        <button onClick={stopTimer} disabled={!isRunning}>
+          Stop
+        </button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Stopwatch;
